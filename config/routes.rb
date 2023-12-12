c912cb4abd98d97adf59ee3976d03033a6df6b80
YASU0210 <150024274+YASU0210@users.noreply.github.com>
2023-12-12 15:13:42 +0000

    投稿

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :books
+  get '/new' => 'books#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'homes/top'
+  get '/top' => 'homes#top'
+  root 'homes#top'
 end
