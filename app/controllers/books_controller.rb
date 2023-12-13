fe7770515e520e049b7d25e283dabd921a7efc05
YASU0210 <150024274+YASU0210@users.noreply.github.com>
2023-12-13 19:23:35 +0000

    削除＆バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,15 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if  @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to '/books'
+    else
+      redirect_to '/books'
+    end
   end
+  
   
   def index
     @books = Book.all
@@ -24,12 +29,14 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)  
   end
   
