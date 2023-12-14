00a82fe3ae0bddb9286bdeada6f80fbc700f5fd6
YASU0210 <150024274+YASU0210@users.noreply.github.com>
2023-12-14 04:15:52 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,47 +3,49 @@ class BooksController < ApplicationController
     @book = Book.new
     @book = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     if  @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to '/books'
     else
-      flash.now[:alert] =
-      render 'new'
+      @books = Book.all
+      render :index
     end
   end
-  
-  
+
+
   def index
     @books = Book.all
-    
+    @book = Book.new
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)  
+    redirect_to book_path(book.id)
+    else
+    render :edit
     end
   end
-  
+
   private
   def book_params
       params.require(:book).permit(:title, :body)
